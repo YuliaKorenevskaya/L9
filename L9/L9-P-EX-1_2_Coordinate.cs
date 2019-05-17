50871855eeaba1025ae7cec96af931ebb661c21a
tegami2007@mail.ru <tegami2007@mail.ru>
2019-05-17 12:49:05 +0300

    Добавьте файлы проекта.

---
+++ L9/L9-P-EX-1_2_Coordinate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9
+{
+    struct Coordinates
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public Coordinates(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public void Deconstruct(out int x1, out int y1, out int z1)
+        {
+            x1 = x;
+            y1 = y;
+            z1 = z;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Random r = new Random();
+            Coordinates xyz;
+            xyz.x = r.Next(1, 11);
+            xyz.y = r.Next(1, 11);
+            xyz.z = r.Next(1, 11);
+
+            int l = (int)Math.Sqrt(xyz.x * xyz.x + xyz.y * xyz.y + xyz.z * xyz.z);
+            Console.WriteLine($"Length = {l};   {xyz.x},{xyz.y},{xyz.z}");
+            Console.ReadKey();
+
+            xyz.Deconstruct(out xyz.x, out xyz.y, out xyz.z);
+        }
+    }
+}
---
+++ L9/L9-P-EX-2_2_DateTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9
+{
+    public static class Dt
+    {          
+        public static void Deconstruct(this DateTime date, out int day, out int month, out int year, out int hour, out int minute, out int second) =>
+        (day, month, year, hour, minute, second) = (date.Day, date.Month, date.Year, date.Hour, date.Minute, date.Second);
+    }
+    class Program
+    {              
+        public static void Main()
+        {
+            DateTime date = DateTime.Now; 
+            Console.WriteLine("Текущая дата: {0}", date);
+            var (_, _, _, hour, minute, second) = date;
+            Console.WriteLine($"Время {hour}:{minute}:{second}");
+            Console.ReadKey();
+        }
+    }
+}
